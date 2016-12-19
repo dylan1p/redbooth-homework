74c677f4ff714b7f0cd36cf3ecd8bf69113ca9b1
Dylan <dyclarke@deloitte.nl>
2016-12-19 10:39:30 +0100

    Update webpack dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
   ],
   devtool: process.env.WEBPACK_DEVTOOL || 'eval-source-map',
   output: {
-    path: path.join(__dirname, 'public'),
+    path: path.join(__dirname, 'dist'),
     filename: 'bundle.js'
   },
   resolve: {
