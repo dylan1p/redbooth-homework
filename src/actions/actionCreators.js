666b12fa0a335f659a701958e87fcb2de1956f94
Dylan <dyclarke@deloitte.nl>
2016-12-19 12:29:44 +0100

    Remove unneeded async functions

--- src/actions/actionCreators.js
+++ src/actions/actionCreators.js
@@ -4,18 +4,18 @@ import * as auth from '../auth';
 import { store } from '../store';
 
 export const authenticateUser = createAction(
-  'AUTHENTICATE_USER', async () => await auth.authenticateUser()
+  'AUTHENTICATE_USER',  () => auth.authenticateUser()
 );
 
 export const changeTaskStatus = createAction(
-  'CHANGE_TASK_STATUS', async (task_id, task_list_id) =>{
+  'CHANGE_TASK_STATUS', (task_id, task_list_id) =>{
     return axios.post(`/api/3/tasks/${task_id}/sort/?access_token=${store.getState().accessToken}`, {
       task_list_id
     }).then(res => res.data);
   });
 
 export const createTask = createAction(
-  'CREATE_TASK', async(task_list_id, project_id, name)=>{
+  'CREATE_TASK', (task_list_id, project_id, name)=>{
     return axios.post(`/api/3/tasks/?access_token=${store.getState().accessToken}`,{
       position: 0,
       task_list_id,
